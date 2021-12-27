ce2f2804bb47916b5bf905959f14c55f2b5e0f0c
Jeeho Ahn <io@fosshost.org>
2021-12-27 09:41:35 +0900

    use requestAnimationFrame

--- pages/[speed].tsx
+++ pages/[speed].tsx
@@ -1,23 +1,35 @@
 import Head from 'next/head'
-import { useState } from 'react'
 import DVDLogo from '../components/dvd'
 import{ useRouter }from 'next/router'
 
-export default function Home() {
+let svgWidth:number
+let svgHeight:number
 
-  const svgWidth = 1000
-  const svgHeight = 800
+export default function Home() {
   const router = useRouter()
+  if (typeof window == 'undefined'){
+    return <Head>
+        <title>dvd logo thing</title>
+        <meta name="description" content="dvd logo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  }
+  svgHeight = window.innerHeight
+  svgWidth = window.innerWidth
   const speed = router.query["speed"];
   if(!speed) {
-    return <></>;
+    return <Head>
+        <title>dvd logo thing</title>
+        <meta name="description" content="dvd logo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
   }
 else{
   return (
     <div className="h-screen flex items-center justify-center">
       <Head>
-        <title>wtf hammy</title>
-        <meta name="description" content="hammy wtf" />
+        <title>dvd logo</title>
+        <meta name="description" content="dvd logo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
      
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,30 @@
 import Head from 'next/head'
-import { useState } from 'react'
 import DVDLogo from '../components/dvd'
 
+let svgWidth:number
+let svgHeight:number
+
 export default function Home() {
-
-  const svgWidth = 1000
-  const svgHeight = 800
-
+  if (typeof window == 'undefined'){
+    return <Head>
+        <title>dvd logo thing</title>
+        <meta name="description" content="dvd logo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  }
+  svgHeight = window.innerHeight
+  svgWidth = window.innerWidth
 
   return (
     <div className="h-screen flex items-center justify-center">
       <Head>
-        <title>wtf hammy</title>
-        <meta name="description" content="hammy wtf" />
+        <title>dvd logo thing</title>
+        <meta name="description" content="dvd logo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
     
       <svg width={svgWidth} height={svgHeight} style={{ backgroundColor: 'black'}}  >
-        <DVDLogo width={svgWidth} height={svgHeight} speed={1} />
+        <DVDLogo width={svgWidth} height={svgHeight} speed={2} />
       </svg>
     </div>
   )
