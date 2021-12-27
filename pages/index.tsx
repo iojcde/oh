ce6b8c7fbfbe2f3393de3865666615b9e2adea37
Jeeho Ahn <io@fosshost.org>
2021-12-27 10:46:36 +0900

    don't use dynamic routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,57 @@
 import Head from 'next/head'
 import DVDLogo from '../components/dvd'
+import{ useRouter }from 'next/router'
+import { useEffect, useState } from 'react'
 
-let svgWidth:number
-let svgHeight:number
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
 export default function Home() {
-  if (typeof window == 'undefined'){
+  const router = useRouter()
+  const [svgWidth,svgHeight] = useWindowSize()
+
+  let { speed } = router.query;
+  useEffect(() => {
+    if (speed == undefined){
+
+      router.push({query:{speed:2}})
+    }
+  })
+
+  if(!speed) {
     return <Head>
         <title>dvd logo thing</title>
         <meta name="description" content="dvd logo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-  }
-  svgHeight = window.innerHeight
-  svgWidth = window.innerWidth
+  } 
+  
+  else{
 
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Head>
-        <title>dvd logo thing</title>
-        <meta name="description" content="dvd logo" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    
-      <svg width={svgWidth} height={svgHeight} style={{ backgroundColor: 'black'}}  >
-        <DVDLogo width={svgWidth} height={svgHeight} speed={2} />
-      </svg>
-    </div>
-  )
+    return (
+      <div>
+        <Head>
+          <title>dvd logo</title>
+          <meta name="description" content="dvd logo" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+     
+        <svg width={svgWidth} height={svgHeight} style={{ backgroundColor: 'black'}}  >
+          <DVDLogo width={svgWidth} height={svgHeight} speed={parseInt(speed as string)} />
+        </svg>
+      </div>
+    )
+  }
 }
+
+export {useWindowSize}
